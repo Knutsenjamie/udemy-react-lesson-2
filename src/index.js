8b4dd4c4a53cb4056e8afa5dc59f4ce48f10a44c
“Jamie <knutsenjamie@yahoo.com>
2021-08-04 11:09:39 -0700

    add big messy block of repeated jsx to show why reusable components are good

--- src/index.js
+++ src/index.js
@@ -9,6 +9,39 @@ const App = () => {
                 <a href="/" className="avatar">
                     <img alt="avatar" src={faker.image.avatar()} />
                 </a>
+                {/* the image tag above shows how you access certain elements from faker js. Simply access faker, then add .(type you want to access) and . (attribute of said type you wish to access) */}
+                <div className="content">
+                    <a href="/" className="author">
+                        Sam
+                    </a>
+                    <div className="metadata">
+                        <span className="date">
+                            Today at 6:00pm
+                        </span>
+                    </div>
+                    <div className="text">Nice Blog Post!</div>
+                </div>
+            </div>
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src={faker.image.avatar()} />
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Sam
+                    </a>
+                    <div className="metadata">
+                        <span className="date">
+                            Today at 6:00pm
+                        </span>
+                    </div>
+                    <div className="text">Nice Blog Post!</div>
+                </div>
+            </div>
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src={faker.image.avatar()} />
+                </a>
                 <div className="content">
                     <a href="/" className="author">
                         Sam
